c016b14745ddd68e6f507aff90c08bae58552d5f
pySoo <soopy368@gmail.com>
2023-04-09 16:35:12 +0900

    feat: localStorage 저장 hook 추가

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue?: T) {
+  const [value, setValue] = useState<T>(() => {
+    let currentValue;
+    try {
+      currentValue = JSON.parse(
+        localStorage.getItem(key) || String(initialValue)
+      );
+    } catch (error) {
+      currentValue = initialValue;
+    }
+
+    return currentValue;
+  });
+
+  useEffect(() => {
+    console.log("get", key, value);
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as const;
+}
